f0048704f2b129a34dc523b5f3f640cd2eac198b
Ugzh <ugo.mateda@gmail.com>
2024-09-18 13:14:26 +0200

    card

--- src/components/CursorTag.tsx
+++ src/components/CursorTag.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react";
 import { Cursor } from "./Cursor";
-export type Color = "pink" | "orange" | "blue" | "purple" | "green" | "yellow";
+import { colorExist, COLOR_TAG_PALLET } from "../utils/colorUtils";
+import type { Color } from "../utils/colorUtils";
 
 interface CursorTagProps {
   href?: string;
@@ -8,67 +9,25 @@ interface CursorTagProps {
   direction?: "right" | "left";
   color: Color;
 }
-const COLOR_PALLET = [
-  {
-    color: "pink",
-    bg: "bg-[#F588B9]",
-    border: "border-[#D6699A]",
-    text: "text-[#F588B9]",
-  },
-  {
-    color: "orange",
-    bg: "bg-[#F28164]",
-    border: "border-[#D46F55]",
-    text: "text-[#F28164]",
-  },
-  {
-    color: "blue",
-    bg: "bg-[#6B93C0]",
-    border: "border-[#6B93C0]",
-    text: "text-[#6B93C0]",
-  },
-  {
-    color: "purple",
-    bg: "bg-[#9FA8FE]",
-    border: "border-[#8A92E3]",
-    text: "text-[#9FA8FE]",
-  },
-  {
-    color: "green",
-    bg: "bg-[#A2B87E]",
-    border: "border-[#95AE6D]",
-    text: "text-[#A2B87E]",
-  },
-  {
-    color: "yellow",
-    bg: "bg-[#CAC48E]",
-    border: "border-[#C2BB82]",
-    text: "text-[#CAC48E]",
-  },
-];
 
 const CursorTag: FC<CursorTagProps> = ({ href, tag, direction, color }) => {
-  const ColorExist = (color: Color) => {
-    return COLOR_PALLET.find((c) => c.color === color);
-  };
-
-  const bgColor = ColorExist(color)?.bg;
-  const cursorColor = ColorExist(color)?.text;
-  const borderColor = ColorExist(color)?.border;
+  const bgColor = colorExist(COLOR_TAG_PALLET, color)?.bg;
+  const cursorColor = colorExist(COLOR_TAG_PALLET, color)?.text;
+  const borderColor = colorExist(COLOR_TAG_PALLET, color)?.border;
 
   const tagDirection =
-    direction === "left" ? "rounded-r-full" : "rounded-l-full";
+    direction === "right" ? "rounded-l-full" : "rounded-r-full";
 
   const tagStyle = `${bgColor} text-[#F3F3F3] inline-block px-4 py-1 rounded-b-full
-  border-2 ${tagDirection} ${borderColor} font-semibold`;
+  border-2 ${tagDirection} ${borderColor} font-semibold text-xs`;
 
   const cursorDirection =
-    direction === "left"
-      ? `absolute -left-4 ${cursorColor} -top-5 rotate-[-10deg] drop-shadow`
-      : `absolute -right-4 ${cursorColor} rotate-[60deg] -top-5 drop-shadow`;
+    direction === "right"
+      ? `absolute -right-3 ${cursorColor} rotate-[60deg] -top-4 drop-shadow`
+      : `absolute -left-3 ${cursorColor} -top-4 rotate-[-10deg] drop-shadow`;
   return href ? (
     <>
-      <a href={href} className={`${tagStyle} relative`}>
+      <a href={href} className={`${tagStyle} relative `}>
         {tag}
         <Cursor className={cursorDirection} />
       </a>
---
+++ src/utils/colorUtils.ts
@@ -0,0 +1,85 @@
+export type Color = "pink" | "orange" | "blue" | "purple" | "green" | "yellow";
+
+export interface ColorPallet {
+  color: Color;
+  bg: string;
+  svg?: string;
+  border?: string;
+  text?: string;
+}
+
+export const COLOR_TAG_PALLET: ColorPallet[] = [
+  {
+    color: "pink",
+    bg: "bg-[#F588B9]",
+    border: "border-[#D6699A]",
+    text: "text-[#F588B9]",
+  },
+  {
+    color: "orange",
+    bg: "bg-[#F28164]",
+    border: "border-[#D46F55]",
+    text: "text-[#F28164]",
+  },
+  {
+    color: "blue",
+    bg: "bg-[#6B93C0]",
+    border: "border-[#6B93C0]",
+    text: "text-[#6B93C0]",
+  },
+  {
+    color: "purple",
+    bg: "bg-[#9FA8FE]",
+    border: "border-[#8A92E3]",
+    text: "text-[#9FA8FE]",
+  },
+  {
+    color: "green",
+    bg: "bg-[#A2B87E]",
+    border: "border-[#95AE6D]",
+    text: "text-[#A2B87E]",
+  },
+  {
+    color: "yellow",
+    bg: "bg-[#CAC48E]",
+    border: "border-[#C2BB82]",
+    text: "text-[#CAC48E]",
+  },
+];
+
+export const COLOR_CARDS: ColorPallet[] = [
+  {
+    color: "pink",
+    bg: "bg-[#FFEDF5]",
+    svg: "bg-[#F588B9]",
+  },
+  {
+    color: "purple",
+    bg: "bg-[#EBEDFF]",
+    svg: "bg-[#9FA8FE]",
+  },
+  {
+    color: "green",
+    bg: "bg-[#ECFFCE]",
+    svg: "bg-[#A2B87E]",
+  },
+  {
+    color: "blue",
+    bg: "bg-[#BADAFF]",
+    svg: "bg-[#6B93C0]",
+  },
+  {
+    color: "yellow",
+    bg: "bg-[#FFFAC2]",
+    svg: "bg-[#CAC48E]",
+  },
+  {
+    color: "orange",
+    bg: "bg-[#FFBCAB]",
+    svg: "bg-[#F28164]",
+  },
+];
+
+export const colorExist = (colorArray: ColorPallet[], color: Color) => {
+  return colorArray.find((c) => c.color === color);
+};
